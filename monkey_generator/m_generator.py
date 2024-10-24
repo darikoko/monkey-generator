1c1d4d38e14cb707fdbac7487a36969cebee48cd
TheoCavinato <theo.cvnt@gmail.com>
2024-10-24 11:48:04 +0200

    ajout de la classe MonkeyAvatar qui devrait permettre de créer ce qu on veut

--- monkey_generator/m_generator.py
+++ monkey_generator/m_generator.py
@@ -1,6 +1,44 @@
 import random
 import os.path
 
+class MonkeyAvatar():
+    def __init__(self, hat=None, glasses=None, mouth=None):
+        self.hat, self.glasses, self.mouth = hat, glasses, mouth
+        self._ressources_p = os.path.abspath(os.path.dirname(__file__)) + "/assets/"
+        self._basic_phenos_p = [ "Tail.xml", "Legs.xml", "Arms.xml", "BodyUpper.xml", "FootLeft.xml", "FootRight.xml", "Ears.xml", "Face.xml", "HandRight.xml", "HandLeft.xml", "Eyes.xml", "Nose.xml" ]
+        self._hats_p = [ None, "CapBackwards.xml", "CapBanano.xml", "CapBebe.xml", "CapCarlos.xml", "CapHngPlus.xml", "CapHng.xml", "CapKappa.xml", "CapPepe.xml", "CapRick.xml", "CapSmugGreen.xml", "CapSmug.xml", "CapThonk.xml", "Cap.xml", "Crown.xml", "HatCowboy.xml", "HatJester.xml", "HelmetViking.xml", "BeanieBanano.xml", "BeanieHippie.xml", "BeanieLongBanano.xml", "BeanieLong.xml", "Beanie.xml" ]
+        self._glasses_p = [ None, "SunglassesAviatorCyan.xml", "SunglassesAviatorGreen.xml", "SunglassesAviatorYellow.xml", "SunglassesThug.xml", "GlassesNerdCyan.xml", "GlassesNerdGreen.xml", "GlassesNerdPink.xml" ]
+        self._mouths_p = [ None, "SmileBigTeeth.xml", "SmileNormal.xml", "SmileTongue.xml", "Confused.xml", "Meh.xml" ]
+
+    def _import_pheno(self, pheno):
+        assert pheno!=None
+        pheno_p = self._ressources_p + pheno
+        pheno_s = open(pheno_p, 'r')
+        pheno_svg = pheno_s.readline()
+        filtered_pheno = pheno_svg[pheno_svg.find('>')+1:pheno_svg.rfind('<')]
+        pheno_s.close()
+
+        return filtered_pheno
+
+    def render(self, out_svg):
+        # basic phenotypes
+        out_data = ['<svg fill="none" height="4000" viewBox="0 0 4000 4000" width="4000" xmlns="http://www.w3.org/2000/svg">']
+
+        for pheno in self._basic_phenos_p:
+            pheno_val = self._import_pheno(pheno)
+            out_data.append(pheno_val)
+
+        # specific phenotypes
+        for pheno in [self.hat, self.glasses, self.mouth]:
+            if pheno: out_data.append(self._import_pheno(pheno))
+
+        # save phenotypes
+        out_svg_s = open(out_svg, 'w')
+        for line in out_data:
+            out_svg_s.write(line)
+        out_svg_s.write('</svg>')
+        out_svg_s.close()
+
 def random_generator(out_svg_p):
 
     MODULE_PATH = os.path.abspath(os.path.dirname(__file__))
